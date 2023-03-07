59873e629fd87b523eb0ab66963546e87b8ccbf9
DerekNguyenn <dereknguyenbusiness@gmail.com>
2023-03-07 12:41:14 -0800

    Initial commit

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function () {
+  let celsius = (f - 32) * (5 / 9);
+  return parseFloat(celsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function () {
+  let fahrenheit = (c * (9 / 5)) + 32;
+  return parseFloat(fahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
