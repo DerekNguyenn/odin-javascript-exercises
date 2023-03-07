449a7fcb6c29d4bd8a0503e9871da6a42fce8d88
DerekNguyenn <dereknguyenbusiness@gmail.com>
2023-03-07 12:05:49 -0800

    Create check to swap arguments when necessary

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,12 @@
 const sumAll = function(a, b) {
+    // Swap arguments if the second passed in is greater than first
+    let temp = 0;
+    if (a > b) {
+        temp = a;
+        a = b;
+        b = temp;
+    }
+
     let sum = 0;
     for (let i = a; i <= b; i++) {
         sum += i;
