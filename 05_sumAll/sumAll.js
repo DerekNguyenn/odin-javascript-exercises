0fd2d228cba5e7c6a850dcb4d40c94a38043f0c9
DerekNguyenn <dereknguyenbusiness@gmail.com>
2023-03-07 12:11:21 -0800

    Separate and condense if-statements to single line

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,9 +1,8 @@
 const sumAll = function(a, b) {
 
     // Check if argument is a non-negative integer
-    if (!Number.isInteger(a) || !Number.isInteger(b) || Math.sign(a) === -1 || Math.sign(b) === -1) {
-        return "ERROR";
-    }
+    if (!Number.isInteger(a) || !Number.isInteger(b)) return "ERROR";
+    if (Math.sign(a) === -1 || Math.sign(b) === -1) return "ERROR";
 
     // Swap arguments if the second passed in is greater than first
     let temp = 0;
